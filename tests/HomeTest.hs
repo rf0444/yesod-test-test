9be1e6a3031d7ef614748bf31d97b7463e66e120
rf <rf0444@gmail.com>
2012-08-19 10:53:02 +0900

    テストを増やした
    コピペだが。

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -6,9 +6,9 @@ module HomeTest
 import TestImport
 
 homeSpecs :: Specs
-homeSpecs =
-  describe "These are some example tests" $
-    it "loads the index and checks it looks right" $ do
+homeSpecs = do
+  describe "These are some example tests 1" $ do
+    it "loads the index and checks it looks right 1-1" $ do
       get_ "/"
       statusIs 200
       htmlAllContain "h1" "Hello"
@@ -22,3 +22,50 @@ homeSpecs =
       htmlCount ".message" 1
       htmlAllContain ".message" "Some Content"
       htmlAllContain ".message" "text/plain"
+    
+    it "loads the index and checks it looks right 1-2" $ do
+      get_ "/"
+      statusIs 200
+      htmlAllContain "h1" "Hello"
+
+      post "/" $ do
+        addNonce
+        fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
+        byLabel "What's on the file?" "Some Content"
+
+      statusIs 200
+      htmlCount ".message" 1
+      htmlAllContain ".message" "Some Content"
+      htmlAllContain ".message" "text/plain"
+  
+  describe "These are some example tests 2" $ do
+    it "loads the index and checks it looks right 2-1" $ do
+      get_ "/"
+      statusIs 200
+      htmlAllContain "h1" "Hello"
+
+      post "/" $ do
+        addNonce
+        fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
+        byLabel "What's on the file?" "Some Content"
+
+      statusIs 200
+      htmlCount ".message" 1
+      htmlAllContain ".message" "Some Content"
+      htmlAllContain ".message" "text/plain"
+    
+    it "loads the index and checks it looks right 2-2" $ do
+      get_ "/"
+      statusIs 200
+      htmlAllContain "h1" "Hello"
+
+      post "/" $ do
+        addNonce
+        fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
+        byLabel "What's on the file?" "Some Content"
+
+      statusIs 200
+      htmlCount ".message" 1
+      htmlAllContain ".message" "Some Content"
+      htmlAllContain ".message" "text/plain"
+
